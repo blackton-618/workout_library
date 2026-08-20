f39f95b339cb6457ae2e5dc89bf58c65d18c924c
Santhosh.S <santhosh12shankar@gmail.com>
2026-08-20 23:29:00 +0530

    frame 2 ui update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function validation() {
+function mainvalidation() {
     let userName = document.getElementById("usernameInput").value.trim();
     let userGender = document.querySelector('input[name="gender"]:checked');
     let userExperience = document.getElementById("experienceSelect").value;
@@ -29,4 +29,34 @@ function validation() {
 
     window.location.href = "F2.html"
     return true;
+}
+
+function F2validation() {
+    let userGoal = document.querySelector('input[name="workout"]:checked');
+    if (userGoal) {
+        userGoal = userGoal.value;
+    } else {
+        window.alert("Please select your workout goal");
+        return false;
+    }
+
+    if (userGoal == "Muscle Build") {
+        window.location.href = "F3.1.html";
+    }
+
+    if (userGoal == "Fat Burn") {
+       window.location.href = "F3.2.html";
+    }
+
+    if (userGoal == "Abs Exercise") {
+        window.location.href = "F3.3.html";
+    }
+
+    if (userGoal == "Calisthenics") {
+        window.location.href = "F3.4.html";
+    }
+
+    console.log("Workout Goal:", userGoal);
+
+    localStorage.setItem("userGoal", userGoal);
 }
